2c6dc86c7dafb5daf1c62b50cdb2cfaf6a229b34
Nico Weingärtner <nico@netztronauten.berlin>
2018-04-03 23:42:28 +0200

    My first commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import Sidebar from './Sidebar'
 import Items from './Items'
+import DetailItem from './DetailItem'
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-     <div className="container">
-         <div className="background animated slideInDown">
-         </div>
-        <Sidebar/>
-         <Items/>
+        <Router>
+            <div className="container">
+                <div className="background animated slideInDown">
+                </div>
+                <Sidebar/>
+                    <Route exact path="/" component={Items} />
+                    <Route  path="/item/:name" component={DetailItem} />
+            </div>
 
-     </div>
+        </Router>
     );
   }
 }
--- src/Item.js
+++ src/Item.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
+import {
+    Link
+} from 'react-router-dom'
 
 class Item extends Component {
     constructor(props) {
         super(props);
-        this.state = {isToggleOn: true};
-        this.toggleClass = this.toggleClass.bind(this);
-        // This binding is necessary to make `this` work in the callback
     }
-    toggleClass(){
-        this.setState(prevState => ({
-            isToggleOn: !prevState.isToggleOn
-        }));
-    };
+
     render() {
         return (
-                <span onClick={this.toggleClass} className={this.state.isToggleOn ? 'item' : 'item animated wobble'}>test</span>
+            <Link className="item" to={`item/${this.props.name}`}>
+               {this.props.name}
+            </Link>
         );
     }
 
